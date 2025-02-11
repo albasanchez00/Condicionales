db2545867cd5d6b7348e1d134d00582f1fbb9b9e
albasanchez00 <albasanchezolmedo00@gmail.com>
2025-02-11 18:39:16 +0100

    Cambios en Actividades Condicionales

--- src/CondicionalIfElse.java
+++ src/CondicionalIfElse.java
@@ -18,11 +18,12 @@ public class CondicionalIfElse {
 
         System.out.println("Ingrese el 2º numero: ");
         int num2 = sc.nextInt();
-        // Importante identificar
+        // En este caso identificamos si son iguales, si no lo son, evaluamos quien es el mayor
+        // ⟦⟦IMPORTANTE⟧⟧ identificar → sangrias
         if(num1==num2){ //Si son iguales
             System.out.print(num1+" y "+num2+ " son iguales");
         } else { //Si son distintos
-            // Variables anidadas.
+            // If-else anidado.
             if(num1>num2){
                 System.out.println("Son distintos y "+ num1+ " es mayor que " + num2);
             } else if (num2>num1) {
