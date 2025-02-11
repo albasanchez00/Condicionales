3bdec08bd2ee911210ee1d843633355656d52f6f
albasanchez00 <albasanchezolmedo00@gmail.com>
2025-02-11 20:02:15 +0100

    Correccion en Actividades Condicionales Cifras

--- src/CondicionalesCifras.java
+++ src/CondicionalesCifras.java
@@ -8,28 +8,28 @@ public class CondicionalesCifras {
         * 2. Solicitamos el numero
         * 3. Declaramos el la variable y leemos el numero.
         *  Si el numero esta comprendido entre 0 y 9 → tiene 1 cifra (num>=0 && numero<10)
-        *  Si el numero esta comprendido entre 10 y 99 → tiene 2 cifras (num>=10 && numero<99)
-        *  Si el numero esta comprendido entre 100 y 999 → tiene 3 cifras (num>=100 && numero<999)
-        *  Si el numero esta comprendido entre 1000 y 9999 → tiene 4 cifras (num>=1000 && numero<9999)
-        *  Si el numero esta comprendido entre 10000 y 99999 → tiene 5 cifras (num>=10000 && numero<99999)
+        *  Si el numero esta comprendido entre 10 y 99 → tiene 2 cifras (num>=10 && numero<10)
+        *  Si el numero esta comprendido entre 100 y 999 → tiene 3 cifras (num>=100 && numero<100)
+        *  Si el numero esta comprendido entre 1000 y 9999 → tiene 4 cifras (num>=1000 && numero<1000)
+        *  Si el numero esta comprendido entre 10000 y 99999 → tiene 5 cifras (num>=10000 && numero<10000)
         */
         Scanner sc = new Scanner(System.in);
         System.out.println("Ingrese un número → ");
         int numero = sc.nextInt();
 
-        if(numero>=0&&numero<=10) {
+        if(numero>=0&&numero<10) {
             System.out.println("El número "+numero+" tiene 1 cifra");
 
-        }else if (numero>=10&&numero<100) {
+        }else if (numero<100) {
             System.out.println("El número "+numero+" tiene 2 cifras");
 
-        }else if (numero>=100&&numero<1000) {
+        }else if (numero<1000) {
             System.out.println("El número "+numero+" tiene 3 cifras");
 
-        }else if (numero>=1000&&numero<10000) {
+        }else if (numero<10000) {
             System.out.println("El número "+numero+" tiene 4 cifras");
 
-        }else if (numero>=10000&&numero<1000000) {
+        }else if (numero<1000000) {
             System.out.println("El número "+numero+" tiene 5 cifras");
         }
     }
