c70dc720010c813eaf9384b8b137cc7a570c0825
albasanchez00 <albasanchezolmedo00@gmail.com>
2025-02-12 18:57:45 +0100

    Actividad Switch Case Notas

---
+++ src/SwitchCaseEdad.java
@@ -0,0 +1,26 @@
+import javax.swing.*;
+import java.util.Scanner;
+
+public class SwitchCaseEdad {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String mensaje;
+        String edadString= JOptionPane.showInputDialog("Ingrese su edad → ");
+        byte edad;
+        try{ // Intenta esto
+            edad=Byte.parseByte(edadString);
+        }catch(Exception e){ // Captura o recoge
+            edad=-1;
+        }
+        switch(edad) {
+            case 1,2,3,4,5,6,7,8,9,10,11 -> mensaje="Eres un niño/a";
+            case 12,13,14,15,16,17 -> mensaje="Eres un adolescente";
+            case 18,19,20,21,22,23,24 -> mensaje="Eres joven";
+            case 25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44 -> mensaje="Eres un adulto joven";
+            case 45,46,47,48,49,50,51,52,53,54,55,56,57,58,59-> mensaje="Eres un adulto";
+            case 60,61,62,63,64,65 -> mensaje="Puedes proceder a la jubilación";
+            default -> mensaje="No es posible reconocer la edad ingresada. Ingrese una edad entre 1 y 65 años";
+        }
+        JOptionPane.showMessageDialog(null,mensaje);
+    }
+}
--- src/SwitchCaseNotas.java
+++ src/SwitchCaseNotas.java
@@ -5,11 +5,17 @@ public class SwitchCaseNotas {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        String notaAlumno= JOptionPane.showInputDialog("Ingrese una nota → ");
-        byte nota=Byte.parseByte(notaAlumno);
-
         String mensaje;
+        String notaAlumno= JOptionPane.showInputDialog("Ingrese una nota → ");
+        byte nota;
+        // Con esto se evita un error de tipo de dato cuando el usuario ingrese la nota.
+        try{ // Intenta esto
+            nota=Byte.parseByte(notaAlumno);
+        }catch(Exception e){ // Captura o recoge
+            nota=-1;
+        }
 
+        // Con la nota indicada, vamos a mostrar un mensaje →
         switch(nota) {
             case 0,1,2,3,4 -> mensaje="Debes estudiar más. Nivel: Suspenso!";
             case 5 -> mensaje="Puedes mejorar. Nivel: Suficiente";
@@ -19,7 +25,6 @@ public class SwitchCaseNotas {
             case 10 -> mensaje="Perfecto. Nivel: Matricula de Honor";
             default -> mensaje="Nota no válida, debe estar comprendida entre 0 y 10 puntos";
         }
-
         JOptionPane.showMessageDialog(null,mensaje);
     }
 }
